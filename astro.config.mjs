97a6c17f2ced944287aab861748d8d4d13448dc3
LilAngelBruh <angel117tutu@gmail.com>
2025-04-10 14:20:54 -0700

    cloudflare integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 import icon from "astro-icon";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
@@ -17,4 +19,6 @@ export default defineConfig({
       },
     }),
   ],
+
+  adapter: cloudflare(),
 });
